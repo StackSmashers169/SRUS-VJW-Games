6b628f6b074030bff3d5f203cc6c1a3a1b81c79b
VicJo <VicJohnathanWilson@outlook.com>
2023-09-30 22:20:57 +0800

    Added proper documentation to player_bst.

--- app/player_bst.py
+++ app/player_bst.py
@@ -8,7 +8,11 @@ class PlayerBST:
         self._root: Optional[PlayerBNode] = None
         self._balancer_list = []
 
-    def insert_leaf(self, player: Player, root: PlayerBNode = None):
+    # adds node to tree, initial call should be to create a new root
+    # then each subsequent call should include the newly created root
+    # from which the method will use recursion to find the next suitable
+    # point to add the node to.
+    def insert_node(self, player: Player, root: PlayerBNode = None):
         key = player.player_name
         if root is None:
             root = PlayerBNode(player)
@@ -20,12 +24,15 @@ class PlayerBST:
             key = player.player_name
 
         if player.player_name < root.player.player_name:
-            root.left = self.insert_leaf(player, root.left)
+            root.left = self.insert_node(player, root.left)
         elif player.player_name > root.player.player_name:
-            root.right = self.insert_leaf(player, root.right)
+            root.right = self.insert_node(player, root.right)
 
         return root
 
+    # takes the root of the tree and a string (key), searches the tree recursively left or right side
+    # depending on the value of the key vs the name associated with root, less than root means searching left side
+    # greater than root and it will search the right side.
     def search_tree(self, root: PlayerBNode, key: str):
         if root is None or root.player.player_name == key:
             return root
@@ -35,8 +42,8 @@ class PlayerBST:
 
         return self.search_tree(root.left, key)
 
-    # helper method that uses DFS inorder traversal
-    # to collect all nodes into balancing list.
+    # uses DFS inorder traversal to collect player objects
+    # from the Binary Tree nodes and append them into the balancer list.
     def collect_nodes(self, root: PlayerBNode = None):
         if root:
             # traverse left side
@@ -52,33 +59,40 @@ class PlayerBST:
 
     # selects the middle element to be the root in the tree
     # so that we can create a balanced binary tree instead
+    # then it uses a recursive call structure similar to merge sort with shifting list positions
+    # represented by start and end as well as local variable mid.
+    # to insert every middle element on either side of the initial middle element
+    # at the end the final element at the end of the list is inserted before the final
+    # function return is made.
     def balance_tree(self, balancer_list: list, start: int, end: int, root: PlayerBNode = None):
         # find the middle of the list and set it as main root
 
         if start >= end:
             # when we reach the last element add it before exiting recursion loop.
             if start == (len(balancer_list) - 1):
-                self.insert_leaf(balancer_list[start], root)
+                self.insert_node(balancer_list[start], root)
             return
 
         mid = start + (end - start) // 2
 
         if root is None:
-            root = self.insert_leaf(balancer_list[mid])
+            root = self.insert_node(balancer_list[mid])
             self._root = root
 
         if balancer_list[mid] < root.player:
-            self.insert_leaf(balancer_list[mid], root)
+            self.insert_node(balancer_list[mid], root)
         elif balancer_list[mid] > root.player:
-            self.insert_leaf(balancer_list[mid], root)
+            self.insert_node(balancer_list[mid], root)
 
         self.balance_tree(balancer_list, start, mid, root)
         self.balance_tree(balancer_list, mid + 1, end, root)
 
+    # getter property for root of the binary search tree.
     @property
     def root(self):
         return self._root
 
+    # setter property for the root of the binary search tree.
     @root.setter
     def root(self, player_bnode: PlayerBNode):
         self._root = player_bnode
@@ -95,12 +109,12 @@ if __name__ == '__main__':
     test_tree = PlayerBST()
 
     # inserting into the tree
-    test_root = test_tree.insert_leaf(test_player_14)
-    test_tree.insert_leaf(test_player_101, test_root)
-    test_tree.insert_leaf(test_player_71, test_root)
-    test_tree.insert_leaf(test_player_86, test_root)
-    test_tree.insert_leaf(test_player_93, test_root)
-    test_tree.insert_leaf(test_player_37, test_root)
+    test_root = test_tree.insert_node(test_player_14)
+    test_tree.insert_node(test_player_101, test_root)
+    test_tree.insert_node(test_player_71, test_root)
+    test_tree.insert_node(test_player_86, test_root)
+    test_tree.insert_node(test_player_93, test_root)
+    test_tree.insert_node(test_player_37, test_root)
     # print unbalanced tree
 
     # collect tree into array
--- test/bst_test.py
+++ test/bst_test.py
@@ -20,41 +20,41 @@ class TestBST(unittest.TestCase):
     def test_initialize_bst(self):
         test_root = PlayerBNode(self.test_player_86)
         test_name = test_root.player_name
-        self.test_tree.root = self.test_tree.insert_leaf(self.test_player_86)
+        self.test_tree.root = self.test_tree.insert_node(self.test_player_86)
         self.assertEqual(test_name, self.test_tree.root.player_name)
 
     # test inserting an element less than the root.
     def test_insert_left(self):
-        first_node = self.test_tree.insert_leaf(self.test_player_86)
-        self.test_tree.root = self.test_tree.insert_leaf(self.test_player_101, first_node)
+        first_node = self.test_tree.insert_node(self.test_player_86)
+        self.test_tree.root = self.test_tree.insert_node(self.test_player_101, first_node)
         left_node = self.test_tree.root.left
         self.assertEqual(self.test_player_101.player_name, left_node.player_name)
 
     def test_insert_right(self):
-        first_node = self.test_tree.insert_leaf(self.test_player_86)
-        self.test_tree.root = self.test_tree.insert_leaf(self.test_player_14, first_node)
+        first_node = self.test_tree.insert_node(self.test_player_86)
+        self.test_tree.root = self.test_tree.insert_node(self.test_player_14, first_node)
         right_node = self.test_tree.root.right
         self.assertEqual(self.test_player_14.player_name, right_node.player_name)
 
     def test_search_pass(self):
-        root = self.test_tree.insert_leaf(self.test_player_14)
-        self.test_tree.insert_leaf(self.test_player_101, root)
-        self.test_tree.insert_leaf(self.test_player_71, root)
-        self.test_tree.insert_leaf(self.test_player_86, root)
-        self.test_tree.insert_leaf(self.test_player_93, root)
-        self.test_tree.insert_leaf(self.test_player_37, root)
+        root = self.test_tree.insert_node(self.test_player_14)
+        self.test_tree.insert_node(self.test_player_101, root)
+        self.test_tree.insert_node(self.test_player_71, root)
+        self.test_tree.insert_node(self.test_player_86, root)
+        self.test_tree.insert_node(self.test_player_93, root)
+        self.test_tree.insert_node(self.test_player_37, root)
 
         found_node = self.test_tree.search_tree(root, 'Gladston')
         # search the tree
         self.assertEqual(found_node.player_name, 'Gladston')
 
     def test_search_fail(self):
-        root = self.test_tree.insert_leaf(self.test_player_14)
-        self.test_tree.insert_leaf(self.test_player_101, root)
-        self.test_tree.insert_leaf(self.test_player_71, root)
-        self.test_tree.insert_leaf(self.test_player_86, root)
-        self.test_tree.insert_leaf(self.test_player_93, root)
-        self.test_tree.insert_leaf(self.test_player_37, root)
+        root = self.test_tree.insert_node(self.test_player_14)
+        self.test_tree.insert_node(self.test_player_101, root)
+        self.test_tree.insert_node(self.test_player_71, root)
+        self.test_tree.insert_node(self.test_player_86, root)
+        self.test_tree.insert_node(self.test_player_93, root)
+        self.test_tree.insert_node(self.test_player_37, root)
 
         self.assertIsNone(self.test_tree.search_tree(root, 'None'))
 
